89108540cf9b707fe243f7f70d42456692f769dc
NanaCs08 <82248322+NanaCs08@users.noreply.github.com>
2024-11-04 18:55:04 -0600

    cambio 2

--- functions/auth.js
+++ functions/auth.js
@@ -3,27 +3,22 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 require('dotenv').config();
 
-// Simulación de datos de usuario
+// Datos de usuario simulados
 const mockUser = {
   username: 'usuarioEjemplo',
   password: 'passwordSeguro'
 };
 
 router.post('/login', (req, res) => {
-  try {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    // Valida las credenciales
-    if (username === mockUser.username && password === mockUser.password) {
-      // Genera un token JWT
-      const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
-      return res.json({ success: true, token });
-    } else {
-      return res.status(401).json({ success: false, message: 'Credenciales incorrectas' });
-    }
-  } catch (error) {
-    console.error('Error en la función de autenticación:', error);
-    return res.status(500).json({ success: false, message: 'Error en el servidor' });
+  // Valida las credenciales
+  if (username === mockUser.username && password === mockUser.password) {
+    // Genera un token JWT
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    return res.json({ success: true, token });
+  } else {
+    return res.status(401).json({ success: false, message: 'Credenciales incorrectas' });
   }
 });
 
